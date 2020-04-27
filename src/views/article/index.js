5cf5cdba2a77bf5d4536ad122b7059c16c25f7b0
tang_jie <583359512@qq.com>
2020-04-27 10:49:36 +0800

    阅读量优化

--- src/views/article/index.js
+++ src/views/article/index.js
@@ -1,29 +1,65 @@
 import React, {Component} from 'react'
-import { Card,Button,Table} from 'antd';
+import { Card,Button,Table,Tag} from 'antd';
 import {getArticle} from '../../request'
+import moment from 'moment'
 
+const titleDisplayMap = {
+    id:"id",
+    title:"标题",
+    amount:"阅读量",
+    author:"作者",
+    currentAt:"创建时间"
+}
 export default class index extends Component{
     state = {
         dataSource:[],
         columns: [],
-        total:0
+        total:0,
+        loading:false
+    }
+    //创建列的代码
+    createColums = columnsKey=>{
+        return columnsKey.map(item=>{
+            if(item === "amount"){
+                return {
+                    title: titleDisplayMap[item],
+                    render:(text,record,index)=>{
+                        let {amount} = record
+                    return <Tag color={amount>200?'pink':'blue'}>{amount}</Tag>
+                    },
+                    key: item,
+                }
+            }
+            if(item === "currentAt"){
+                return {
+                    title: titleDisplayMap[item],
+                    render:(text,record,index)=>{
+                        let {currentAt} = record
+                        return moment(currentAt).format("YYYY年MM月DD日 HH:mm:ss")
+                    },
+                    key: item,
+                }
+            }
+            return {
+                title: titleDisplayMap[item],
+                dataIndex: item,
+                key: item,
+            }
+        })
     }
     getData = ()=>{
+        this.setState({loading:true})
         getArticle().then(res=>{
             console.log("article",res)
             const columnsKey = Object.keys(res.list[0])
-            const columns = columnsKey.map(item=>{
-                return {
-                    title: item,
-                    dataIndex: item,
-                    key: item,
-                }
-            })
+            const columns =this.createColums(columnsKey)
             this.setState({
                 total:res.total,
                 columns,
                 dataSource:res.list
             })
+        }).finally(()=>{
+            this.setState({loading:false})
         })
     }
     componentDidMount(){
@@ -39,6 +75,7 @@ export default class index extends Component{
                 dataSource={this.state.dataSource} 
                 columns={this.state.columns} 
                 rowKey={record=>record.id}
+                loading={this.state.loading}
                 pagination={
                    {
                         total:this.state.total
